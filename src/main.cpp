ef4062ee9c6dc0fa304f10e9423ae7e405fa5c92
Daniel Heinlein <daniel.heinlein@aalto.fi>
2022-08-11 16:07:19 +0300

    changing the documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-/// This is the main function.
+/// This is the main function, it only shows Hello World.
 int main(int argc, char const *argv[]){
 	std::cout << "Hello World" << '\n';
 	return 0;
