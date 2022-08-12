ce6ab1d613c44338909df66ed49fc52c8a52367a
Daniel Heinlein <daniel.heinlein@aalto.fi>
2022-08-12 11:55:41 +0300

    working on coverage

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 /// This is the main function, it only shows Hello World.
 int main(int argc, char const *argv[]){
 	std::cout << "Hello World\n";
-	if(argc == 2){
-		std::cout << "Hello World false\n";
-	}
+	// if(argc == 2){
+	// 	std::cout << "Hello World false\n";
+	// }
 	return 0;
 }
